981b28347e4577390e674f934623a66f04a3c8d1
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-30 23:15:34 +0530

    login page is added

---
+++ src/components/authentication/Login.js
@@ -0,0 +1,44 @@
+import { useRef } from 'react';
+import classes from './Login.module.css';
+
+const Login = (props) => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const loginHandler = (event) => {
+        event.preventDefault();
+
+        const enteredEmail = emailRef.current.value;
+        const enteredPassword = passwordRef.current.value;
+        props.onLogin(enteredEmail, enteredPassword);
+    }
+
+    return (
+        <section>
+            <div className={classes.login}>
+            <h1>Login</h1>
+                <form onSubmit={loginHandler}>
+                    <input 
+                        name='email'
+                        id='email'
+                        type='email'
+                        placeholder='Email'
+                        required
+                        ref={emailRef}
+                    />
+                    <input 
+                        name='password'
+                        id='password'
+                        type='password'
+                        placeholder='Password'
+                        required
+                        ref={passwordRef}
+                    />
+                    <button>Login</button>
+                </form>
+            </div>
+        </section>
+    )
+};
+
+export default Login;
