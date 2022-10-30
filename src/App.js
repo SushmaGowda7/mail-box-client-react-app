a858c77a2e9e7f738c86f790dfae6089b73782de
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-30 23:14:51 +0530

    Sign up page is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from 'react';
+import Login from './components/authentication/Login';
+import SignUp from './components/authentication/SignUp';
 
 function App() {
   return (
     <Fragment>
-    
+      <SignUp />
     </Fragment>
   );
 }
